5c7349063b46a02c0701d3f045282369f5b34211
Wang Nie <wang.nie@cnsdc01.pwc.com>
2024-08-17 23:48:47 +0800

    feat(005): add jwt auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,17 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './controller/auth.controller';
 import { UserModule } from '../modules/user';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constant';
 
 @Module({
   imports: [
-    forwardRef(() => UserModule)
+    forwardRef(() => UserModule),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' }
+    })
   ],
   controllers: [AuthController],
 })
--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -2,10 +2,14 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from '../../modules/user';
 import { UserDTO } from '../../modules/user';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly userService: UserService) {}
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService
+    ) {}
 
     @Post('login')
     async login(@Body('email') email: string, @Body('user') user: UserDTO) {
@@ -13,7 +17,7 @@ export class AuthController {
         if(exist) {
             const match = await bcrypt.compare(user.password, exist.password);
             if (match) {
-                return { success: true, login_success: true };
+                return { success: true, login_success: true, access_token: await this.jwtService.signAsync({ id: exist.id, email: exist.email }) };
             } else {
                 return { success: false, login_success: false };
             }
