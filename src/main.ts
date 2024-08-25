e5b5e4c29c37c6e45402b3a772de9aef56060961
explorerNW <32231114+explorerNW@users.noreply.github.com>
2024-08-25 18:46:32 +0800

    feat(017): add compression (#5)

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,16 @@ import * as dotenv from 'dotenv';
 import { LoggingInterceptor } from './interceptors';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import helmet from 'helmet';
-import * as csurf from 'csurf';
 import * as cookieParser from 'cookie-parser';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import fastifyCsrf from '@fastify/csrf-protection';
 import { LoggerService } from './modules/logger/logger.service';
+import fastifyCookie from '@fastify/cookie';
+import compression from '@fastify/compress';
+import { constants } from 'zlib';
 dotenv.config();
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
@@ -22,10 +23,13 @@ async function bootstrap() {
   app.enableVersioning({ type:VersioningType.MEDIA_TYPE, key: 'v=' });
   app.use(helmet());
   app.enableCors();
-  // app.use(cookieParser());
-  // app.use(csurf({ cookie: true }));
+  app.use(cookieParser());
   app.useLogger(app.get(LoggerService));//  ['error', 'warn']
+  await app.register(fastifyCookie, {
+    secret: 'my-secret'
+  });
   await app.register(fastifyCsrf);
+  await app.register(compression, { encodings: ['gzip', 'deflate'], brotliOptions: { params: { [constants.BROTLI_PARAM_QUALITY]: 4 } } });
   await app.listen(process.env.SERVER_PORT, ()=>{
     console.log(`server start on: ${process.env.SERVER_PORT}`);
   });
