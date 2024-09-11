d92bff913c54c3217ca13b099cadc2805cecb1f2
explorerNW <superexplorernw@163.com>
2024-09-11 11:14:05 +0800

    feat(015): rename micro-service/basic to micro-service/tcp

--- micro-service/basic/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  accumuate(data: number[] = []): number {
-    return data.reduce((a, b) => a+b, 0);
-  }
-}
