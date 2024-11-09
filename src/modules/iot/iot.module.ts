dd5421c897cbea881b391fa64bbbbaa18b9e4377
explorerNW <superexplorernw@163.com>
2024-11-09 18:13:22 +0800

    feat(031): enhancement - add authGuard for iot module

--- src/modules/iot/iot.module.ts
+++ src/modules/iot/iot.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { IotController } from './controller';
 import { IotService } from './service';
 import { HttpModule } from '@nestjs/axios';
+import { AuthModule } from '../../auth';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, AuthModule],
   controllers: [IotController],
   providers: [IotService],
 })
