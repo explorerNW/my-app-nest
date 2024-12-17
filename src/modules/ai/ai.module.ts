6af49f64c53e2e6ef392e48ef26e91d9e6c19e01
explorerNW <superexplorernw@163.com>
2024-12-17 15:51:32 +0800

    feat(044): add azure openai

--- src/modules/ai/ai.module.ts
+++ src/modules/ai/ai.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AiService } from './service';
 import { AiController } from './controllers/ai.controller';
+import { AZureOpenAiService } from './service/ai.azure.service';
 
 @Module({
-  providers: [AiService],
+  providers: [AiService, AZureOpenAiService],
   controllers: [AiController],
 })
 export class AiModule {}
--- src/modules/ai/controllers/ai.controller.ts
+++ src/modules/ai/controllers/ai.controller.ts
@@ -1,12 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AiService } from '../service';
+import { AZureOpenAiService } from '../service/ai.azure.service';
 
 @Controller('ai')
 export class AiController {
-  constructor(private aiService: AiService) {}
+  constructor(
+    private aiService: AiService,
+    private aZureOpenAiService: AZureOpenAiService,
+  ) {}
 
   @Post('chat')
   prompt(@Body('prompt') prompt: string) {
     return this.aiService.prompt(prompt);
+  }
+
+  @Post('azure/chat')
+  azurePrompt(@Body('prompt') prompt: string) {
+    return this.aZureOpenAiService.ask(prompt);
   }
 }
